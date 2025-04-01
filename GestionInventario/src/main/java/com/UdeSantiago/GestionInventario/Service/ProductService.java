bce69a084dc3e9ae06b81c94995a6465409bfcad
Santiago <santiude2004@gmail.com>
2025-04-01 15:40:00 -0300

    Create ProductService.java

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Service/ProductService.java
@@ -0,0 +1,6 @@
+package com.UdeSantiago.GestionInventario.Service;
+
+import com.UdeSantiago.GestionInventario.Model.ProductDTO;
+import org.springframework.util.ReflectionUtils;
+import com.UdeSantiago.GestionInventario.Exception.ResourceNotFoundException;
+import com.UdeSantiago.GestionInventario.Model.Product;
