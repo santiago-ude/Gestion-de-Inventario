4d0af542dc3870a0146dc1cd0e03ef4968cf89df
Santiago <santiude2004@gmail.com>
2025-04-01 15:40:16 -0300

    Update ProductController.java

--- GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Controller/ProductController.java
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Controller/ProductController.java
@@ -7,3 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
