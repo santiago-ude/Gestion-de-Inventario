80eb7712a4ec220ba59ea713d965034f52e0cfc5
Santiago <santiude2004@gmail.com>
2025-04-01 15:39:12 -0300

    Create ProductRepository.java

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.UdeSantiago.GestionInventario.Repository;
+
+import com.UdeSantiago.GestionInventario.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
