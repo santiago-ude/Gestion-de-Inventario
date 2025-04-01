a824dd1e92a99b25b1025bce64c8de074eec383c
santiago ude <107772887+santiago-ude@users.noreply.github.com>
2025-04-01 10:34:12 -0300

    Add files via upload

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/GestionDeInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.UdeSantiago.GestionInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeInventarioApplication.class, args);
+	}
+
+}
---
+++ GestionInventario/src/test/java/com/UdeSantiago/GestionInventario/GestionDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UdeSantiago.GestionInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
