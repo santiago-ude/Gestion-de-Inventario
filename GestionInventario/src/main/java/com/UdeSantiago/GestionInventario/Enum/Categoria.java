30d1a4598bae42d3382629f6749f58cf6ed56e44
Santiago <santiude2004@gmail.com>
2025-04-01 15:38:58 -0300

    Create Categoria.java

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Enum/Categoria.java
@@ -0,0 +1,12 @@
+package com.UdeSantiago.GestionInventario.Enum;
+
+public enum Categoria {
+    ELECTRONICA,
+    ROPA,
+    ALIMENTOS,
+    HOGAR,
+    LIMPIEZA,
+    CUIDADO_PERSONAL,
+    BEBIDA
+
+}
