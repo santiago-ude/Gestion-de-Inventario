1431db55b3d5b9c610af7b1b5c586836f7c24343
Santiago <santiude2004@gmail.com>
2025-04-02 00:13:36 -0300

    Create product-update.component.ts

---
+++ Front/gestion-inventario/src/app/products/components/product-update/product-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-update',
+  imports: [],
+  templateUrl: './product-update.component.html',
+  styleUrl: './product-update.component.css'
+})
+export class ProductUpdateComponent {
+
+}
