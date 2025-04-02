0df4aa142e8a11b45c76e66c60d838ca166c4dd8
Santiago <santiude2004@gmail.com>
2025-04-01 23:08:19 -0300

    Update ProductService.java

--- GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Service/ProductService.java
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Service/ProductService.java
@@ -4,3 +4,71 @@ import com.UdeSantiago.GestionInventario.Model.ProductDTO;
 import org.springframework.util.ReflectionUtils;
 import com.UdeSantiago.GestionInventario.Exception.ResourceNotFoundException;
 import com.UdeSantiago.GestionInventario.Model.Product;
+import com.UdeSantiago.GestionInventario.Repository.ProductRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository PR;
+
+    public List<Product> getProducts(){
+        return PR.findAll();
+    }
+
+
+    public Product getProductById(Long id){
+        return PR.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Producto con ID " + id + " no encontrado"));
+    }
+
+    public Product postProduct(Product newProduct){
+
+    return PR.save(newProduct);
+    }
+
+    public Product putProduct(Long id, ProductDTO newProduct){
+
+        return PR.findById(id)
+                .map(aux -> {
+                    BeanUtils.copyProperties(newProduct, aux, "id");
+                    return PR.save(aux);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Producto con ID " + id + " no encontrado"));
+    }
+
+
+    public Product patchProduct(Long id, Map<String, Object> updates) {
+        return PR.findById(id)
+                .map(existingProduct -> {
+                    updates.forEach((key, value) -> {
+                        Field field = ReflectionUtils.findField(Product.class, key);
+                        if (field != null) {
+                            field.setAccessible(true);
+                            ReflectionUtils.setField(field, existingProduct, value);
+                        }
+                    });
+                    return PR.save(existingProduct);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Producto con ID " + id + " no encontrado"));
+    }
+
+    public void deleteProduct(Long id){
+
+        if(!PR.existsById(id)){
+            throw new ResourceNotFoundException("Producto con ID " + id + " no encontrado");
+        }
+        PR.deleteById(id);
+    }
+
+
+
+}
