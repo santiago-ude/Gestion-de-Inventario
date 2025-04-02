35e024e2cfbb90b47c7bf925bf49ec418bcc6d09
Santiago <santiude2004@gmail.com>
2025-04-02 00:13:34 -0300

    Create product.ts

---
+++ Front/gestion-inventario/src/app/products/interface/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+
+
+    id?: Number,
+    name: String,
+    description: String,
+    price: Number,
+    stock: Number,
+    brand: String,
+    imageUrl: String;
+
+
+}
