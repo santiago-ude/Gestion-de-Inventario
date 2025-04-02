aff679c89ffeeb3f1234ba3a69d946a81403e481
Santiago <santiude2004@gmail.com>
2025-04-02 00:13:31 -0300

    Create product-add.component.ts

---
+++ Front/gestion-inventario/src/app/products/components/product-add/product-add.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Product } from '../../interface/product';
+
+@Component({
+  selector: 'app-product-add',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './product-add.component.html',
+  styleUrl: './product-add.component.css'
+})
+export class ProductAddComponent {
+
+
+  @Output()
+  emitirProduct: EventEmitter<Product> = new EventEmitter();
+
+FB = inject(FormBuilder);
+
+
+
+formulario = this.FB.nonNullable.group(
+  {
+    name: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+    description: ["", [Validators.required, Validators.maxLength(500)]],
+    price : [0,[Validators.required]],
+    stock : [0,[Validators.required]],
+    brand : ["", [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+    imageUrl : ["", [Validators.required]]
+  }
+)
+addProduct = () =>{
+
+  console.log(this.formulario.errors); // Verifica si hay errores generales
+  for (const controlName in this.formulario.controls) {
+      const control = this.formulario.get(controlName);
+      console.log(`${controlName} errors:`, control?.errors); // Muestra errores de cada campo
+  }
+
+  if(this.formulario.invalid)return;
+
+
+  const product = this.formulario.getRawValue();
+  this.formulario.reset();
+
+  this.emitirProduct.emit(product);
+
+}
+
+
+}
