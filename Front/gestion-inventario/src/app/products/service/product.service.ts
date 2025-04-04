07e2a574b1f74162e8d52417fc36b929a0a84dcb
Santiago <santiude2004@gmail.com>
2025-04-04 00:41:33 -0300

    Update product.service.ts

--- Front/gestion-inventario/src/app/products/service/product.service.ts
+++ Front/gestion-inventario/src/app/products/service/product.service.ts
@@ -3,3 +3,11 @@ import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Product } from '../interface/product';
 import { ProductAddComponent } from '../components/product-add/product-add.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+   private apiUrl = "http://localhost:8080/inventoryManagment/api/v1/products";
+
