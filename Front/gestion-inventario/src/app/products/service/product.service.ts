ea7d63b5ac2293940ff6302008091c885f26f145
Santiago <santiude2004@gmail.com>
2025-04-04 00:41:38 -0300

    Update product.service.ts

--- Front/gestion-inventario/src/app/products/service/product.service.ts
+++ Front/gestion-inventario/src/app/products/service/product.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Product } from '../interface/product';
-import { ProductAddComponent } from '../components/product-add/product-add.component';
 
 @Injectable({
   providedIn: 'root'
