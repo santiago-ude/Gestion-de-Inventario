cbe4cd2cb14e61194d42dd546d7dca9df090d0f9
Santiago <santiude2004@gmail.com>
2025-04-05 18:11:07 -0300

    Create app.component.ts

---
+++ Front/gestion-inventario/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./landingPage/menu/menu.component";
+import { ProductAddComponent } from './products/components/product-add/product-add.component';
+import { ProductUpdateComponent } from './products/components/product-update/product-update.component';
+
+@Component({
