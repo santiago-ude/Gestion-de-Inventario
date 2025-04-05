d2d0109c0ace85130496f35b1d30681b842bdf9f
Santiago <santiude2004@gmail.com>
2025-04-05 18:10:44 -0300

    Update product-update.component.ts

--- Front/gestion-inventario/src/app/products/components/product-update/product-update.component.ts
+++ Front/gestion-inventario/src/app/products/components/product-update/product-update.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductService } from '../../service/product.service';
+import { Product } from '../../interface/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-update',
