70e10fcfc39c56502833d8698b5487a81a288377
Santiago <santiude2004@gmail.com>
2025-04-05 18:11:12 -0300

    Update app.config.ts

--- Front/gestion-inventario/src/app/app.config.ts
+++ Front/gestion-inventario/src/app/app.config.ts
@@ -1,4 +1,5 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
