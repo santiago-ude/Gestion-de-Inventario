28c9897831a3168fd90e6343811fa53c58b53f84
Santiago <santiude2004@gmail.com>
2025-04-07 01:39:17 -0300

    Update app.routes.ts

--- Front/gestion-inventario/src/app/app.routes.ts
+++ Front/gestion-inventario/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductAddComponent } from './products/components/product-add/product-add.component';
+import { ProductListComponent } from './products/components/product-list/product-list.component';
+import { ProductUpdateComponent } from './products/components/product-update/product-update.component';
 
 export const routes: Routes = [];
