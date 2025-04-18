1088256d3ff21cd1536e88420af1f610e4e40484
Santiago <santiude2004@gmail.com>
2025-04-17 22:52:29 -0300

    Update ConfigSwagger.java

--- GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Config/ConfigSwagger.java
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Config/ConfigSwagger.java
@@ -4,3 +4,5 @@ package com.UdeSantiago.GestionInventario.Config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
