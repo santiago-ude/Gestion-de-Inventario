822877a83106d566a6b2c3de933e32cc2b937a6d
Santiago <santiude2004@gmail.com>
2025-04-24 23:54:40 -0300

    Create SupplierController.java

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Controller/SupplierController.java
@@ -0,0 +1,8 @@
+package com.UdeSantiago.GestionInventario.Controller;
+
+import com.UdeSantiago.GestionInventario.Model.Product;
+import com.UdeSantiago.GestionInventario.Model.Supplier;
+import com.UdeSantiago.GestionInventario.Service.SupplierService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
