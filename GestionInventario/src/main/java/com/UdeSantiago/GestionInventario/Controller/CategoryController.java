1c562e2c94b1faad7aee2166cf571b7e84618969
Santiago <santiude2004@gmail.com>
2025-04-24 23:54:00 -0300

    .

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Controller/CategoryController.java
@@ -0,0 +1,7 @@
+package com.UdeSantiago.GestionInventario.Controller;
+
+import com.UdeSantiago.GestionInventario.Model.Category;
+import com.UdeSantiago.GestionInventario.Service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
