4380923a2652998abb533a8327d0e5cd08d3ca5a
Santiago <santiude2004@gmail.com>
2025-04-25 01:19:41 -0300

    Create SupplierRepository.java

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.UdeSantiago.GestionInventario.Repository;
+
+import com.UdeSantiago.GestionInventario.Model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
