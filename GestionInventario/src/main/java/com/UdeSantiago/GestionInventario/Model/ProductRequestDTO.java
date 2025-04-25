1f5d22a281e15a933eafc766369226114dfa3e3b
Santiago <santiude2004@gmail.com>
2025-04-25 01:19:47 -0300

    Create ProductRequestDTO.java

---
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Model/ProductRequestDTO.java
@@ -0,0 +1,93 @@
+package com.UdeSantiago.GestionInventario.Model;
+
+import java.math.BigDecimal;
+
+public class ProductRequestDTO {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private String brand;
+    private String imageUrl;
+    private Long categoryId;
+    private Long supplierId;
+
+    public ProductRequestDTO() {
+    }
+
+    public ProductRequestDTO(String name, String description, BigDecimal price, Integer stock, String brand, String imageUrl, Long categoryId, Long supplierId) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.brand = brand;
+        this.imageUrl = imageUrl;
+        this.categoryId = categoryId;
+        this.supplierId = supplierId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Long supplierId) {
+        this.supplierId = supplierId;
+    }
+}
