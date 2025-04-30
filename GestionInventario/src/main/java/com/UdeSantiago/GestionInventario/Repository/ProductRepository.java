147321b406b147560cac74429c98ada30f105bbd
Santiago <santiude2004@gmail.com>
2025-04-30 17:23:26 -0300

    Update ProductRepository.java

--- GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Repository/ProductRepository.java
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Repository/ProductRepository.java
@@ -3,6 +3,9 @@ package com.UdeSantiago.GestionInventario.Repository;
 import com.UdeSantiago.GestionInventario.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
