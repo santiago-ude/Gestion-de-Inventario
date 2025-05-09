665f9eb6708873f37953b20314f24ab3f53f1c27
Santiago <santiude2004@gmail.com>
2025-05-09 00:03:45 -0300

    Update ProductRequestDTO.java

--- GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Model/ProductRequestDTO.java
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Model/ProductRequestDTO.java
@@ -17,6 +17,10 @@ public class ProductRequestDTO {
     }
 
     
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
     public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
     }
