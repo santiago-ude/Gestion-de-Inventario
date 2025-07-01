27c4bac49b86f4f68487dcbb2f12af95cee9cb26
Santiago <santiude2004@gmail.com>
2025-06-30 22:47:30 -0300

    Update ProductRequestDTO.java

--- GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Model/ProductRequestDTO.java
+++ GestionInventario/src/main/java/com/UdeSantiago/GestionInventario/Model/ProductRequestDTO.java
@@ -18,10 +18,6 @@ public class ProductRequestDTO {
 
 
 
-    public String getBrand() {
-        return brand;
-    }
-
     public void setBrand(String brand) {
         this.brand = brand;
     }
